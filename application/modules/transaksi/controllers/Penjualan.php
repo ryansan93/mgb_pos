eb40551e6943e7706d169c0beeb4432970a62582
ryansan93 <ryansantoso93@gmail.com>
2022-08-31 09:57:13 +0700

    error pembayaran

--- application/modules/transaksi/controllers/Penjualan.php
+++ application/modules/transaksi/controllers/Penjualan.php
@@ -658,7 +658,7 @@ class Penjualan extends Public_Controller
             $printer -> text("$lineKasir\n");
 
             if ( $this->config->item('print_jenis_bayar') == 1 ) {
-                $jenis_bayar = ($data['bayar']['jenis_bayar'] == 'tunai') ? 'TUNAI' : $data['bayar'][ count($data['bayar']) -1 ]['jenis_kartu']['nama'];
+                $jenis_bayar = ($data['bayar'][0]['jenis_bayar'] == 'tunai') ? 'TUNAI' : $data['bayar'][ count($data['bayar']) -1 ]['jenis_kartu']['nama'];
                 $lineBayar = sprintf('%-13.5s %1.05s %-13.40s','Bayar',':', $jenis_bayar);
                 $printer -> text("$lineBayar\n");
             }
